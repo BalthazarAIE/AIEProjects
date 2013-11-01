9d9a0f242e551224c795b67da3c0f75baed21bbb
BalthazarAIE <justinl@aie.edu.au>
2013-10-31 17:52:19 -0700

    unity work

---
+++ UnityProjects/Prototyping/Assets/Scripts/CameraSelection.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSelection : MonoBehaviour {
+	
+	public Camera cam;
+	
+	public Transform HitObject; 
+	
+	
+	RaycastHit hit;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+		
+		
+		
+		if(Physics.Raycast(ray, out hit,1000f))
+		{
+			Debug.DrawLine(transform.position,hit.point,Color.green,.1f);
+			HitObject = hit.collider.transform;
+		}
+	
+		
+	
+	}
+}
---
+++ UnityProjects/Prototyping/Assets/Scripts/GridMapCreator.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridMapCreator : MonoBehaviour {
+	
+	
+	public int gridXSize=2;
+	public int gridYSize=2;
+	public int gridObjectSize = 10;
+	
+	public GameObject[] Tiles;
+	
+	public GameObject Tile;
+	
+	private Vector3 tempPosition = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+		Tiles = new GameObject[gridXSize*gridYSize];
+		CreateGrid();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void CreateGrid()
+	{
+		int j =0;
+		for(int i = 0; i < gridXSize; i++)
+		{
+			
+
+			for(j = 0; j < gridYSize; j++)
+			{
+				Tiles[i *(j +1)]= Instantiate(Tile,tempPosition,Quaternion.identity) as GameObject;
+				Tiles[i *(j +1)].name = "Tile( " + i +" , " + j + " )";
+				tempPosition.z += gridObjectSize;
+			}
+			tempPosition.z = 0;
+			tempPosition.x += gridObjectSize;
+			
+		} 
+		
+	}
+	
+}
