f044561b81248f409e88d8e6fbb69d742dfde33e
BalthezalAIE <justinl@aie.edu.au>
2014-01-22 16:44:44 -0800

    dfasdf

---
+++ OpenGL4Tut/OpenGL4Tut/source/Sprite.cpp
@@ -0,0 +1,30 @@
+#include "Sprite.h"
+#include <TerrehByteMath.h>
+
+Sprite::Sprite(void)
+{
+
+	Quad[0].Pos = tbyte::Vector4(-0.25f,0.25f,0.0f,1.0f);
+	Quad[1].Pos = tbyte::Vector4(-0.25f,-0.25f,0.0f,1.0f);
+	Quad[2].Pos = tbyte::Vector4(0.25f,0.25f,0.0f,1.0f);
+	Quad[3].Pos = tbyte::Vector4(0.25f,-0.25f,0.0f,1.0f);
+
+	Quad[0].Color = tbyte::Vector4(1.0,0.0f,1.0f,1.0f);
+	Quad[1].Color = tbyte::Vector4(1.0,0.0f,1.0f,1.0f);
+	Quad[2].Color = tbyte::Vector4(1.0,0.0f,1.0f,1.0f);
+	Quad[3].Color = tbyte::Vector4(1.0,0.0f,1.0f,1.0f);
+
+	Quad[0].UV = tbyte::Vector2(0.0,0.0f);
+	Quad[1].UV = tbyte::Vector2(0.0,1.0f);
+	Quad[2].UV = tbyte::Vector2(1.0,0.0f);
+	Quad[3].UV = tbyte::Vector2(1.0,1.0f);
+
+
+}
+
+
+Sprite::~Sprite(void)
+{
+}
+
+void Sprite
